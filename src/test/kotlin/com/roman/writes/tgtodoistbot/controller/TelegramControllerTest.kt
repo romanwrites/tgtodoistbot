11c1b8f0d9631f56d9e398a1efb0fefb19f39732
romanwrites <thekukinpower@gmail.com>
2023-03-09 03:13:48 +0400

    optimize imports

--- src/test/kotlin/com/roman/writes/tgtodoistbot/controller/TelegramControllerTest.kt
+++ src/test/kotlin/com/roman/writes/tgtodoistbot/controller/TelegramControllerTest.kt
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.telegram.telegrambots.meta.api.objects.Message
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
