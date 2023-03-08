35e4de50bf8667cef3628bd6cdd4b0058d9b3005
romanwrites <thekukinpower@gmail.com>
2023-03-09 03:30:14 +0400

    add logger

--- src/main/kotlin/com/roman/writes/tgtodoistbot/controller/TelegramController.kt
+++ src/main/kotlin/com/roman/writes/tgtodoistbot/controller/TelegramController.kt
@@ -1,15 +1,17 @@
 package com.roman.writes.tgtodoistbot.controller;
 
+import com.roman.writes.tgtodoistbot.service.TelegramService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.objects.Message
 
 @RestController
-class TelegramController {
+class TelegramController(private val telegramService: TelegramService) {
 
     @PostMapping("/telegram/update")
     fun update(message: Message): ResponseEntity<String> {
+        telegramService.update(message)
         return ResponseEntity.ok("OK");
     }
 }
---
+++ src/main/kotlin/com/roman/writes/tgtodoistbot/service/TelegramService.kt
@@ -0,0 +1,16 @@
+package com.roman.writes.tgtodoistbot.service
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.telegram.telegrambots.meta.api.objects.Message
+
+@Service
+class TelegramService {
+
+    val log: Logger = LoggerFactory.getLogger(TelegramService::class.java)
+
+    fun update(message: Message) {
+        log.info("add logger")
+    }
+}
